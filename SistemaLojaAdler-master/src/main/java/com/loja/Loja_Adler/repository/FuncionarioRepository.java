169e0cf0d23c8ee771227992e261e0fd1b1dfc8f
AdlerCachuba <adlercachuba@gmail.com>
2021-07-27 22:01:32 -0300

    Até aula 11 completo

---
+++ SistemaLojaAdler-master/src/main/java/com/loja/Loja_Adler/repository/FuncionarioRepository.java
@@ -0,0 +1,9 @@
+package com.loja.Loja_Adler.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.loja.Loja_Adler.model.Funcionario;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+}
