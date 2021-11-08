db272154131e32272ca562a15448f6ee758dc4f3
AdlerCachuba <adlercachuba@gmail.com>
2021-11-08 01:09:05 -0300

    Feito corretamente inserção da funcionalidade de buscar por descrição,buscar por marca e por categoria.

--- SistemaLojaAdler-master/src/main/java/com/loja/Loja_Adler/repository/CategoriaRepository.java
+++ SistemaLojaAdler-master/src/main/java/com/loja/Loja_Adler/repository/CategoriaRepository.java
@@ -3,7 +3,12 @@ package com.loja.Loja_Adler.repository;
 
 import com.loja.Loja_Adler.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+    @Query("from Categoria c where c.nome like %?1% ")
+    List<Categoria> findByNome(String nome);
 
 }
